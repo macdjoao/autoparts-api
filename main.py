e624aa49f1bd255037943323335fd80379f72400
João Aguiar <macdjoao@gmail.com>
2024-10-12 15:32:45 -0300

    feat: adiciona endpoint para listagem de usuários e detalhe de usuários

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
+import uuid
+
 from fastapi import FastAPI
 
 from constants import run_settings
 
 app = FastAPI()
 
+users = {
+    1: {
+        'id': str(uuid.uuid4()),
+        'email': 'arthur@email.com',
+        'first_name': 'arthur',
+        'last_nanme': 'morgan',
+        'password': 'pw',
+        'role': 'admin'
+    },
+    2: {
+        'id': str(uuid.uuid4()),
+        'email': 'john@email.com',
+        'first_name': 'john',
+        'last_nanme': 'marston',
+        'password': 'pw',
+        'role': 'staff'
+    },
+    3: {
+        'id': str(uuid.uuid4()),
+        'email': 'jack@email.com',
+        'first_name': 'jack',
+        'last_nanme': 'marston',
+        'password': 'pw',
+        'role': 'staff'
+    },
+}
+
 
 @app.get('/ping')
 def ping():
     return {'detail': 'pong'}
+
+
+@app.get('/users')
+def get_users():
+    return users
+
+
+@app.get('/users/{id}')
+def get_user(id: int):
+    return users[id]
 
 
 if __name__ == '__main__':
