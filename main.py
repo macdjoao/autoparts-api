721a31dbf42be2179ed22a16ecf3ff3349b0fe09
João Aguiar <macdjoao@gmail.com>
2024-10-12 18:00:35 -0300

    feat: adiciona tratamento de erros

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
 import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 from constants import run_settings
+from schemas import User
 
 app = FastAPI()
 
+# Nomearei o identificador com "pk", para evitar possíveis problemas, pois "id" é um palavra reservada do Python.
 users = {
     1: {
-        'id': str(uuid.uuid4()),
+        'pk': str(uuid.uuid4()),  # Esse valor deve ser gerado pelo SGBD
         'email': 'arthur@email.com',
         'first_name': 'arthur',
-        'last_nanme': 'morgan',
+        'last_name': 'morgan',
         'password': 'pw',
         'role': 'admin'
     },
     2: {
-        'id': str(uuid.uuid4()),
+        'pk': str(uuid.uuid4()),
         'email': 'john@email.com',
         'first_name': 'john',
-        'last_nanme': 'marston',
+        'last_name': 'marston',
         'password': 'pw',
         'role': 'staff'
     },
     3: {
-        'id': str(uuid.uuid4()),
+        'pk': str(uuid.uuid4()),
         'email': 'jack@email.com',
         'first_name': 'jack',
-        'last_nanme': 'marston',
+        'last_name': 'marston',
         'password': 'pw',
         'role': 'staff'
     },
 }
 
 
-@app.get('/ping')
-def ping():
-    return {'detail': 'pong'}
-
-
+# Padrão de nomenclatura dos endpoints: "/resources".
+# "/" + nome do recurso (substantivo) em que a operação está sendo realizada, sem "/" no final.
+# Tanto singular "/resource", quanto plural "/resources", são aceitos, mas é importante seguir o padrão escolhido para todos os endpoints.
 @app.get('/users')
 def get_users():
-    return users
+    try:
+        return users
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='Internal Server Error'
+        )
 
 
-@app.get('/users/{id}')
-def get_user(id: int):
-    return users[id]
+@app.get('/users/{pk}')
+def get_user(pk: int) -> User:
+    try:
+        return users[pk]
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with pk {pk} not found'
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='Internal Server Error'
+        )
 
 
 if __name__ == '__main__':
