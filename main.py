deb5f7df2537eb48b61672798f2c7f47fd4a4e29
João Aguiar <macdjoao@gmail.com>
2024-10-12 12:06:58 -0300

    chore: adiciona o uso de variáveis de ambiente no projeto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from constants import run_settings
 
 app = FastAPI()
 
@@ -11,4 +12,10 @@ def ping():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app=app)
+    uvicorn.run(
+        # TODO descobrir porque quando " reload=True ", não se pode usar " app=app ", sendo necessário o uso de " app='main:app' ".
+        app='main:app',
+        host=run_settings.get('HOST'),
+        port=run_settings.get('PORT'),
+        reload=run_settings.get('RELOAD')
+    )
