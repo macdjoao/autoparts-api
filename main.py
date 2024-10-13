1d4fb2fcb51c1a8708425f2030768709bff5f2e8
João Aguiar <macdjoao@gmail.com>
2024-10-13 18:43:31 -0300

    docs: adiciona mais informações sobre o uso de uuid

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from schemas import User
 app = FastAPI()
 
 # Nomearei o identificador com "pk", para evitar possíveis problemas, pois "id" é um palavra reservada do Python.
-# Usarei "uuid" como chave primária, em vez de "int auto increment", por questões de segurança da aplicação; Esse valor deve ser gerado pelo SGBD.
+# Usarei "uuid" como chave primária, em vez de "int auto increment", esse valor deve ser gerado pelo BD.
+# Ao usar uuid, perdemos peformance (um select realizado em uma coluna do tipo integer é mais rápido do que em uma coluna do tipo string/uuid);
+# Tambem perdemos um pouco de armazenamento, já que uma string/uuid precisa de mais espaço para armazenamento do que um integer;
+# No entanto, ganhamos em segurança, uma vez que dados como o número de registros de uma tabela não será facilmente identificado por agentes maliciosos.
 users = {
     1: {
         'pk': str(uuid.uuid4()),
