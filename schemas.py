b7c15203b29151548089c1980fb11b94d82b04fd
João Aguiar <macdjoao@gmail.com>
2024-10-14 18:30:03 -0300

    feat: realiza alguns ajustes e validacoes em alguns campo do schema User

--- schemas.py
+++ schemas.py
@@ -1,18 +1,25 @@
 from datetime import datetime
 from typing import Optional
+import uuid
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class User(BaseModel):
-    pk: str
+    # Para trafegar dados do tipo data, usarei strings com formatação ISO-8601, padrão que RESTful segue
+    pk: Optional[str] = str(uuid.uuid4)
     is_active: Optional[bool] = True
     created_by: Optional[str] = None
-    created_at: Optional[datetime] = None
+    created_at: Optional[str] = datetime.now().isoformat()
     updated_by: Optional[str] = None
-    updated_at: Optional[datetime] = None
-    email: str
+    updated_at: Optional[str] = datetime.now().isoformat()
+    email: EmailStr
     first_name: str
     last_name: str
     password: str
     role: str
+
+    @field_validator('first_name', 'last_name')
+    @classmethod
+    def capitalize_names(cls, v: str) -> str:
+        return v.capitalize()
