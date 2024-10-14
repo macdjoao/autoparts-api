246db9f4ec0c5092fecb382abd6960988a9a9b11
João Aguiar <macdjoao@gmail.com>
2024-10-14 18:27:18 -0300

    feat: adiciona algumas possibilidades de uso do JSONResponse para os retornos dos endpoints

--- routes/users.py
+++ routes/users.py
@@ -2,13 +2,15 @@ from typing import List
 
 from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+# from fastapi import Path, Query, Header       TODO: Fazer alguns testes com essas funções
 
 from schemas import User
 from models import users
 
 
 router = APIRouter(
-    prefix='/api/v1',
+    prefix='/users',
     tags=['Users']
 )
 
@@ -17,7 +19,7 @@ router = APIRouter(
 # "/" + nome do recurso (substantivo) em que a operação está sendo realizada, sem "/" no final.
 # Tanto singular "/resource", quanto plural "/resources", são aceitos, mas é importante seguir o padrão escolhido para todos os endpoints.
 @router.get(
-    '/users',
+    '',
     # Passar "-> List[User]" como retorno na assinatura da função teria o mesmo efeito que response_model
     response_model=List[User],
     status_code=status.HTTP_200_OK,
@@ -28,7 +30,7 @@ router = APIRouter(
 async def get_users():
     try:
         # status_code padrão é 200, estou explicitando só para frisar a existência do parâmetro
-        return JSONResponse(content=users, status_code=status.HTTP_200_OK)
+        return JSONResponse(content=jsonable_encoder(users), status_code=status.HTTP_200_OK)
     except Exception as exc:
         print(exc)
         raise HTTPException(
@@ -38,7 +40,7 @@ async def get_users():
 
 
 @router.get(
-    '/users/{pk}',
+    '/{pk}',
     response_model=User,
     status_code=status.HTTP_200_OK,
     summary='Busca usuário',
@@ -46,12 +48,35 @@ async def get_users():
 )
 async def get_user(pk: int):
     try:
-        return JSONResponse(content=users[pk], status_code=status.HTTP_200_OK)
-    except KeyError:
+        user = users[pk-1]
+        response = User(**user).model_dump()
+        return JSONResponse(content=response, status_code=status.HTTP_200_OK)
+    except IndexError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'User with pk {pk} not found'
         )
+    except Exception as exc:
+        print(exc)
+        print(type(exc))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail='Internal Server Error'
+        )
+
+
+@router.post(
+    '',
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary='Cadastra usuário',
+    description='Cadastra um novo usuário no sistema.'
+)
+async def post_user(user: User):
+    try:
+        users.append(user)
+        response: User = users[-1]
+        return JSONResponse(content=response.model_dump(), status_code=status.HTTP_201_CREATED)
     except Exception as exc:
         print(exc)
         raise HTTPException(
