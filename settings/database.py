0a795b7a797dd954e9b5e3d51a2e8ddb55b248bc
João Aguiar <macdjoao@gmail.com>
2024-10-15 14:56:25 -0300

    chore: adiciona configuracao para abrir/fechar sessao com banco de dados

---
+++ settings/database.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine, Session
+
+from settings.settings import settings
+
+
+engine = create_engine(settings.DB_URL, echo=True)
+session = Session(engine)
+
+
+def get_session():
+    try:
+        yield session
+    finally:
+        session.close()
