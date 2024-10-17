c5b8005da118129ed00e3f5f4e3cad830583e0dd
João Aguiar <macdjoao@gmail.com>
2024-10-17 20:42:12 -0300

    docs: atualiza comentarios sobre como executar um app FastAPI

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.include_router(router.router)
 # Adicionando esse trecho de código ao fim do arquivo main, ao chamar "$ python3 main.py",
 # temos o mesmo efeito que "$ uvicorn main:app --port 8000 --reload".
 # Outra forma de executar a aplicação, pelo menos em ambiente de dev, é usando "$ fastapi dev main.py"
+# UPDATE: A forma correta de executar um app FastAPI é "$ fastapi dev main.py" em ambiente de desenvolvimento, e "$ fastapi run main.py" em ambiente de produção.
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(
