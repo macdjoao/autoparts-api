cbdeae93821d9c33ca8502284ae93e4a826a4862
João Aguiar <macdjoao@gmail.com>
2024-10-19 23:18:03 -0300

    feat: adiciona funcoes de autenticacao e criacao de token

--- utils/security.py
+++ utils/security.py
@@ -1,7 +1,15 @@
+from datetime import datetime, timedelta, timezone
+
+from fastapi.security import OAuth2PasswordBearer
+import jwt
 from passlib.context import CryptContext
+
+from models.users import User
+from settings.settings import settings
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/token")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -10,3 +18,21 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def authenticate_user(user: User, password: str):
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
