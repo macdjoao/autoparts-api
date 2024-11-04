0d81ce5f0384974cdd3c4d48e5d809161b1ca254
João Aguiar <macdjoao@gmail.com>
2024-11-04 20:02:04 -0300

    test: adiciona teste post user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,40 @@
-from fastapi.testclient import TestClient
 from fastapi import status
 
-from main import app
 from settings.settings import settings
 
-client = TestClient(app)
 
-
-def test_get_users():
-    client = TestClient(app)
+def test_get_users(client):
 
     response = client.get(f'{settings.URI_PREFIX}/users')
-
     status_code = response.status_code
     content = response.json()
 
     assert status_code == status.HTTP_200_OK
     assert isinstance(content, list)
+
+
+def test_get_user():
+    pass
+
+
+def test_post_user(client, fake):
+
+    url = f'{settings.URI_PREFIX}/users'
+    json = {
+        'email': fake.email(),
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'password': fake.password()
+    }
+    response = client.post(url=url, json=json)
+    status_code = response.status_code
+    content = response.json()
+
+    assert status_code == status.HTTP_201_CREATED
+    assert 'email' in content
+    assert 'first_name' in content
+    assert 'last_name' in content
+    assert 'pk' in content
+    assert 'is_active' in content
+    assert 'created_at' in content
+    assert 'updated_at' in content
