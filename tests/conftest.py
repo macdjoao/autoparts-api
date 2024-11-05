0874926a12edb1a6fb7fa3337a3586903491b2d6
João Aguiar <macdjoao@gmail.com>
2024-11-05 19:05:13 -0300

    chore: configura fixture para usar banco de dados em memoria para realizacao dos testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,43 @@
 import pytest
 from fastapi.testclient import TestClient
 from faker import Faker
+from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel.pool import StaticPool
 
 from main import app
+from utils.database import get_session
+
+
+# @pytest.fixture(name='fixture_name')
+# Pode-se usar o parametro "name" para definir o nome da fixture e usá-la nos testes,
+# do contrário deve-se usar o nome da função da fixture
+@pytest.fixture
+def session():
+    engine = create_engine(
+        'sqlite://', connect_args={'check_same_thread': False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def fake():
-    return Faker(locale='pt_BR')
+    return Faker()
+
+
+@pytest.fixture
+def users_url():
+    return '/api/v1/users'
