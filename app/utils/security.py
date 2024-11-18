860f5070195a6fd3dde2c9e0a30604f3df3e7932
João Aguiar <macdjoao@gmail.com>
2024-11-18 17:00:53 -0300

    feat: adiciona duas funcoes auxiliares que serao utilizadas nos processos de autenticacao e login

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,11 +1,17 @@
 from datetime import datetime, timedelta, timezone
+from typing import Annotated
 
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from passlib.context import CryptContext
+from sqlmodel import Session, select
 
+from app.models.token import TokenData
 from app.models.users import User
 from app.settings.settings import settings
+from app.utils.dependencies import get_session
+from app.utils.exceptions import raise_credentials_exception
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -32,7 +38,34 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         expire = datetime.now(timezone.utc) + expires_delta
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
+    to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    session: Session = Depends(get_session)
+):
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        username = payload.get('sub')
+        if username is None:
+            raise_credentials_exception()
+        token_data = TokenData(email=username)
+    except jwt.InvalidTokenError:
+        raise_credentials_exception()
+    db_user = session.exec(select(User).where(
+        User.email == token_data.email)).first()
+    if db_user is None:
+        raise_credentials_exception()
+    return db_user
+
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail='Inactive user')
+    return current_user
