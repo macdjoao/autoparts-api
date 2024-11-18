47b19eaf377d5337de2fce0adc4a8c4c90b851c3
João Aguiar <macdjoao@gmail.com>
2024-11-18 17:02:46 -0300

    fix: remove import inutilizado

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from passlib.context import CryptContext
