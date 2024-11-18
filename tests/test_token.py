2825fb01a951755dd8f0cead841e12803e29e1ab
João Aguiar <macdjoao@gmail.com>
2024-11-18 14:47:17 -0300

    test: adiciona testes para endpoint token

---
+++ tests/test_token.py
@@ -0,0 +1,45 @@
+from app.utils.security import get_password_hash
+
+
+token_url = '/api/v1/auth/token'
+
+
+def test_post_token_202_accepted(client, create_specific_user, fake):
+
+    email = fake.email()
+    password = fake.password()
+
+    create_specific_user(
+        email=email,
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+        hashed_password=get_password_hash(password)
+    )
+
+    json = {
+        'email': email,
+        'password': password
+    }
+
+    response = client.post(url=token_url, json=json)
+    status_code = response.status_code
+    content = response.json()
+
+    assert status_code == 202
+    assert isinstance(content.get('access_token'), str)
+    assert content.get('token_type') == 'bearer'
+
+
+def test_post_token_401_incorrect_email_or_password(client, fake):
+
+    json = {
+        'email': fake.email(),
+        'password': fake.password()
+    }
+
+    response = client.post(url=token_url, json=json)
+    status_code = response.status_code
+    content = response.json()
+
+    assert status_code == 401
+    assert content.get('detail') == 'Incorrect email or password'
