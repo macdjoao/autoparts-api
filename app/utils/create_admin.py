c1ad891890bcfe2c97242d92dabe35c87f06ea45
João Aguiar <macdjoao@gmail.com>
2024-11-19 19:11:18 -0300

    feat: adiciona automacao para criacao de usuario admin

--- app/utils/create_admin.py
+++ app/utils/create_admin.py
@@ -1,20 +1,25 @@
+from sqlmodel import select
+
 from app.models.users import User
 from app.security.auth import get_password_hash
 from app.utils.settings import settings
 from app.utils.dependencies import session
 
 
-def create_admin():
-    admin = User(
-        email=settings.ADMIN_EMAIL,
-        first_name='Admin',
-        last_name='Admin',
-        hashed_password=get_password_hash(settings.ADMIN_PASSWORD),
-        is_admin=True
-    )
-    session.add(admin)
-    session.commit()
-    session.refresh(admin)
+def register_admin():
 
-
-create_admin()
+    admin_already_registered = session.exec(
+        select(User).where(User.email == settings.ADMIN_EMAIL)).first()
+    if admin_already_registered or settings.CREATE_ADMIN == False:
+        pass
+    else:
+        admin = User(
+            email=settings.ADMIN_EMAIL,
+            first_name='Admin',
+            last_name='Admin',
+            hashed_password=get_password_hash(settings.ADMIN_PASSWORD),
+            is_admin=True
+        )
+        session.add(admin)
+        session.commit()
+        session.refresh(admin)
