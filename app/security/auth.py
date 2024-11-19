f0d8b57b11d0f2ffabc9309bcaa9781ba49a4bd8
João Aguiar <macdjoao@gmail.com>
2024-11-19 19:51:01 -0300

    feat: adiciona permissao personalizada is_admin

--- app/security/auth.py
+++ app/security/auth.py
@@ -68,3 +68,10 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     if not current_user.is_active:
         raise HTTPException(status_code=400, detail='Inactive user')
     return current_user
+
+
+async def is_admin(current_user: User = Depends(get_current_active_user)):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=403, detail='Only admin users can perform this action')
+    return current_user
