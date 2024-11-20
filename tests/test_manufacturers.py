4f35ed2f1fb2006f598bd128039fa8a89ac4e32e
João Aguiar <macdjoao@gmail.com>
2024-11-20 15:44:07 -0300

    test: adiciona teste de sucesso para post manufacturers

--- tests/test_manufacturers.py
+++ tests/test_manufacturers.py
@@ -21,3 +21,22 @@ def test_manufacturers_get_list(client, create_manufacturer, token):
     assert 'updated_by' in manufacturer
     assert 'name' in manufacturer
     assert 'is_active' in manufacturer
+
+
+def test_manufacturers_post(client, token, fake):
+
+    headers = {'Authorization': f'Bearer {token()}'}
+    name = fake.word()
+    json = {'name': name}
+
+    response = client.post(url=manufacturers_url, headers=headers, json=json)
+    content = response.json()
+
+    assert response.status_code == 201
+    assert content['name'] == name.capitalize()
+    assert 'pk' in content
+    assert 'is_active' in content
+    assert 'created_at' in content
+    assert 'updated_at' in content
+    assert 'created_by' in content
+    assert 'updated_by' in content
