10a52b6ce973fb2de9059f06da5f52f26d599474
João Aguiar <macdjoao@gmail.com>
2024-11-20 17:24:42 -0300

    feat: adiciona endpoint de atualizacao parcial patch manufacturer

--- app/routers/manufacturers.py
+++ app/routers/manufacturers.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import APIRouter, HTTPException, Query, status, Depends
 from sqlmodel import Session, select
 
-from app.models.manufacturers import ManufacturerPublic, Manufacturer, ManufacturerCreate, ManufacturerUpdate
+from app.models.manufacturers import ManufacturerPartialUpdate, ManufacturerPublic, Manufacturer, ManufacturerCreate, ManufacturerUpdate
 from app.models.users import User
 from app.utils.dependencies import get_session
 from app.utils.exceptions import raise_internal_server_error_exception, raise_name_already_registered_exception, raise_pk_not_found_exception
@@ -131,6 +131,49 @@ async def put_manufacturer(
 
         manufacturer_data = manufacturer.model_dump()
 
+        db_manufacturer.sqlmodel_update(manufacturer_data)
+
+        session.add(db_manufacturer)
+        session.commit()
+        session.refresh(db_manufacturer)
+
+        return db_manufacturer
+
+    except HTTPException as exc:
+        raise exc
+    except Exception:
+        raise_internal_server_error_exception()
+
+
+@router.patch(
+    '/{pk}',
+    response_model=ManufacturerPublic,
+    status_code=status.HTTP_202_ACCEPTED,
+    summary='Atualiza parcialmente uma fabricante',
+    description='Atualiza parcialmente uma fabricante previamente cadastrada no sistema.'
+)
+async def patch_manufacturer(
+    pk: UUID,
+    manufacturer: ManufacturerPartialUpdate,
+    current_user: User = Depends(get_current_active_user),
+    session: Session = Depends(get_session)
+):
+    try:
+
+        db_manufacturer = session.get(Manufacturer, pk)
+        if not db_manufacturer:
+            raise_pk_not_found_exception(pk=pk)
+
+        if manufacturer.name:
+            name_already_registered = session.exec(select(Manufacturer).where(
+                Manufacturer.name == manufacturer.name, Manufacturer.pk != pk)
+            ).first()
+            if name_already_registered:
+                raise raise_name_already_registered_exception(
+                    name=manufacturer.name
+                )
+
+        manufacturer_data = manufacturer.model_dump(exclude_unset=True)
         db_manufacturer.sqlmodel_update(manufacturer_data)
 
         session.add(db_manufacturer)
