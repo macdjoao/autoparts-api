d2c40fb64fdf208c96a0ba5488b48d1f11802be3
João Aguiar <macdjoao@gmail.com>
2024-11-26 18:03:35 -0300

    chore: adiciona novas variaveis de ambiente de cache em settings

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -10,23 +10,36 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file='.env')
 
+    # Framework
     APP: str = 'main:app'
     APP_VERSION: str
     APP_HOST: str
     APP_PORT: int
     APP_RELOAD: bool
     URI_PREFIX: str
+
+    # Database
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 1 * 1  # (minutos * horas * dias)
+
+    # JWT
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 1  # (minutos * horas * dias)
     ALGORITHM: str
     SECRET_KEY: str
+
+    # Admin
     CREATE_ADMIN: bool
     ADMIN_EMAIL: str
     ADMIN_PASSWORD: str
+
+    # Cache
+    CACHE_HOST: str
+    CACHE_PORT: int = 6379
+    CACHE_DB: int
+    CACHE_TIME_TO_EXP: int = 60 * 60 * 1  # (segundos * minutos * horas)
 
     @property
     def DB_URL(self) -> str:
